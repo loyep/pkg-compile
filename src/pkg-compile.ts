388f5e63ebe3e9dd42bcf91f055f228b39d8c9d3
Zack <mwl@live.com>
2022-07-12 16:14:56 +0800

    Update pkg-compile.ts

--- src/pkg-compile.ts
+++ src/pkg-compile.ts
@@ -2,7 +2,6 @@ import minimist from 'minimist';
 import { build, BuildOption } from '.';
 
 export async function run() {
-    1;
     const argv = minimist<BuildOption>(process.argv.slice(2), {
         boolean: ['fix', 'no-default'],
         string: ['dict', 'disable', 'ignore', 'presets'],
